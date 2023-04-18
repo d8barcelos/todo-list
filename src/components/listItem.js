e4e0651b7670c1a5d5bea59d5736220a72bf80e5
Diogo <d8barcelos@gmail.com>
2023-04-18 15:53:23 -0300

    style changes

--- src/components/listItem.js
+++ src/components/listItem.js
@@ -1,28 +1,37 @@
 import React from "react";
 import Card from "./card";
 
-function DoneImg(props) {
-
-    if (props.done) {
-        return (<img className="check" alt="done" src="./assets/switchon.png"></img>)
-    } else {
-        return (<img className="notCheck" alt="undone" src="./assets/switchoff.png"></img>)
-    }
+function DoneCheckbox(props) {
+    return (
+        <label className="checkbox">
+            <input
+                type="checkbox"
+                checked={props.done}
+                onChange={() => {
+                    props.onDone(props.item);
+                }}
+            />
+            <span></span>
+        </label>
+    );
 }
 
 function ListItem(props) {
-
     return (
-        
-            <li key={props.item.id}>
-            <Card className={props.item.done? "done item" : "item" }>
-            {props.item.text}
-            <div className="icons">
-            <button onClick={()=> {props.onDone(props.item)} }><DoneImg done={props.item.done}></DoneImg></button>
-            <button onClick={()=> {props.onItemDeleted(props.item)} }><img className="delete" src="./assets/delete.svg" alt="img"></img></button>
-            </div>
+        <li key={props.item.id}>
+            <Card className={props.item.done ? "done item" : "item"}>
+                {props.item.text}
+                <div className="controls">
+                <DoneCheckbox done={props.item.done} onDone={props.onDone} item={props.item} />
+                <div>
+                <button className="delete" onClick={() => { props.onItemDeleted(props.item) }}>x</button>
+                </div>
+                </div>
             </Card>
-            </li>)}
-        
+        </li>
+    );
+}
+
+
 
 export default ListItem;
